8db1aa5d4c9ddbdbebb858f5e952b93332b4a629
shanehsi <shane.hsi@foxmail.com>
2024-03-18 10:01:16 +0800

    build: add src to files of npm pakcages for debug experience; exports more functions for unit test

---
+++ packages/generator/src/generator.ts
@@ -0,0 +1,8 @@
+export { ExtendedDMMF } from './classes';
+export { loadDMMF } from './classes/__tests__/utils/loadDMMF';
+export { parseGeneratorConfig } from './utils/parseGeneratorConfig';
+export { generateSingleFile } from './generateSingleFile';
+export { generateMultipleFiles } from './generateMultipleFiles';
+export { writeModelFiles } from './functions/writeMultiFileModelFiles';
+export { writeModelOrType } from './functions/contentWriters/writeModelOrType';
+export { FileWriter } from './classes/fileWriter';
